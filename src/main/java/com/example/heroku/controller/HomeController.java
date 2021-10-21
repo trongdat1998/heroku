9c4efac6b4cb284e7df952fb77445beb275e4361
trongdat1998 <68001618+trongdat1998@users.noreply.github.com>
2021-10-21 20:25:22 +0700

    vnpay

--- src/main/java/com/example/heroku/controller/HomeController.java
+++ src/main/java/com/example/heroku/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ import java.util.List;
 public class HomeController {
     @Autowired
     AccountDao ac;
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public String home(Model model){
         List<Account> a = ac.findAll();
         model.addAttribute("mesage",a.get(0).getUsername());
